b7cc62afdb4b9cffe3420e3cf13c1c1339ba1e52
unknown <caiolopesfv@gmail.com>
2025-09-04 18:33:42 -0300

    Criando crud de usuário + rota de login

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Prisma, User as UserModel } from 'generated/prisma';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userSErvice: UserService) {}
+
+  @Post('user')
+  async signUpUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userSErvice.createUser(userData);
+  }
+}
