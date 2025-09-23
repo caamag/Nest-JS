b35e4b36899dec697ca76ca0fad3e0ae56a6d990
unknown <caiolopesfv@gmail.com>
2025-09-23 19:06:35 -0300

    Adicionando validações na tipagem

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString()
+  @MinLength(5, { message: 'O nome precisa ter no mínimo cinco caracteres.' })
+  @IsNotEmpty({ message: 'Nome não pode estar vazio.' })
   readonly name: string;
+
+  @MaxLength(10, { message: 'A desrição não pode ter mais que 10 caracteres.' })
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
 }
