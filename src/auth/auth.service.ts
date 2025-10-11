e98340ce2255238a917910f095d0aff84ba5a63b
unknown <caiolopesfv@gmail.com>
2025-10-10 22:08:35 -0300

    Creating token when login was done successfuly

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,20 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, Inject, Injectable } from '@nestjs/common';
 import { SignInDto } from './dto/signIn.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { HashingServiceProtocol } from './hash/hash.service';
+import { type ConfigType } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     private prisma: PrismaService,
     private readonly hash: HashingServiceProtocol,
+
+    @Inject(jwtConfig.KEY)
+    private readonly jwt: ConfigType<typeof jwtConfig>,
+    private readonly jwtService: JwtService,
   ) {}
 
   async authenticate(signInData: SignInDto) {
@@ -27,11 +34,25 @@ export class AuthService {
     if (!passwordIsValid)
       throw new HttpException('Email e/ou senha incorreta', 400);
 
+    const token = await this.jwtService.signAsync(
+      {
+        sub: String(user.id),
+        email: user.email,
+      },
+      {
+        secret: this.jwt.secret,
+        expiresIn: this.jwt.jwtTtl as any,
+        audience: this.jwt.audience,
+        issuer: this.jwt.issuer,
+      },
+    );
+
     return {
       message: 'Usuário logado com sucesso',
       data: {
         name: user.name,
         email: user.email,
+        token: token,
       },
     };
   }
